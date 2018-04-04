089d19996acca0b80b2f69fb439505a1a75c8d9e
Dhruv Gupta <dhruv4@gmail.com>
2018-04-04 17:10:31 -0400

    we done

--- P3/dhruv.py
+++ P3/dhruv.py
@@ -9,6 +9,7 @@ import numpy
 import torch
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
 
 SAMPLE_RATE = 22050 # number of samples of the audio per second
 
@@ -20,6 +21,26 @@ def eval(pred, actual):
             count += 1
 
     return count / len(actual)
+
+def extract_feature(X, sample_rate, BINS_OCTAVE, NUM_BINS):
+    # X, sample_rate = librosa.load(file_name)
+    stft = np.abs(librosa.stft(X))
+    mfccs = np.mean(librosa.feature.mfcc(y=X, sr=sample_rate, n_mfcc=40).T,axis=0)
+    chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
+    mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
+    contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
+    sr=sample_rate).T,axis=0)
+
+    ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
+
+    return np.array(ext_features)
+
+def write_to_file(filename, predictions):
+    with open(filename, "w") as f:
+    	f.write("Id,Prediction\n")
+        for i, p in enumerate(predictions):
+            f.write(str(i) + "," + str(p) + "\n")
 
 # Given a wav time series, makes a mel spectrogram
 # which is a short-time fourier transform with
@@ -36,7 +57,7 @@ def tempogram(y, SAMPLE_RATE):
 
 def main():
 
-	N = 10000
+	N = None
 
 	# Just some re-shaping and dimension finding
 
@@ -52,9 +73,10 @@ def main():
 
 	test = test[:,1:-1]
 
-	X_train = train[:,1:-2] # remove first and last two elements to drop the mic
 	y_train = train[:,-1]
 	y_train = y_train.reshape(N_train,1)
+
+	X_train = train[:,1:-2] # remove first and last two elements to drop the mic
 
 	# JUST SOME FOURIER TRANSFORM PARAMETERS
 	BINS_OCTAVE = 12*2
@@ -63,17 +85,39 @@ def main():
 
 	# This means that the spectrograms are 168 rows (frequencies)
 	# By 173 columns (time frames)
-	song = X_train[0]
-	test_spec = mel_spec(song, SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-	print test_spec.shape
-	FEATS = test_spec.shape[0]
-	FRAMES = test_spec.shape[1]
+	# song = X_train[0]
+	# test_spec = mel_spec(song, SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+	# print test_spec.shape
+	# FEATS = test_spec.shape[0]
+	# FRAMES = test_spec.shape[1]
 
-	print "features"
+	# print "features"
 
-	tmp_train = np.zeros((N_train,FEATS,FRAMES))
-	for i in range(N_train):
-	    tmp_train[i,:,:] = mel_spec(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+	# tmp_train = np.zeros((N_train,FEATS,FRAMES))
+	# for i in range(N_train):
+	#     tmp_train[i,:,:] = mel_spec(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+
+	# tmp_train = np.zeros((N_train,193))
+
+	# for i in range(N_train):
+	# 	f = extract_feature(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+	# 	# print f
+	# 	tmp_train[i] = np.array(f)
+
+	tmp_test = np.zeros((N_test,193))
+
+	for i in range(N_test):
+		f = extract_feature(test[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+		# print f
+		tmp_test[i] = np.array(f)
+
+	# np.save("features-train-dhruv.npy", tmp_train)
+
+	np.save("features-test-dhruv.npy", tmp_test)
+
+	tmp_train = np.load("features-train-dhruv.npy")
+
+	tmp_test = np.load("features-test-dhruv.npy")
 
 	X_train, X_valid, y_train, y_valid = train_test_split(tmp_train, y_train, test_size=0.2)
 
@@ -85,17 +129,39 @@ def main():
 
 	print X_train.shape
 
-	nsamples, nx, ny = X_train.shape
-	d2_train_dataset = X_train.reshape((nsamples,nx*ny))
+	# nsamples, nx, ny = X_train.shape
+	# d2_train_dataset = X_train.reshape((nsamples,nx*ny))
 
-	nsamples, nx, ny = X_valid.shape
-	d2_valid_dataset = X_valid.reshape((nsamples,nx*ny))
+	# nsamples, nx, ny = X_valid.shape
+	# d2_valid_dataset = X_valid.reshape((nsamples,nx*ny))
 
-	regr.fit(d2_train_dataset, y_train)
+	# regr.fit(X_train, y_train)
 
-	valid_pred = regr.predict(d2_valid_dataset)
+	# valid_pred = regr.predict(X_valid)
 
-	print "Evaluation score:", eval(valid_pred, y_valid)
+	# print "Lin Reg Evaluation score:", eval(valid_pred, y_valid)
+
+	gb = GradientBoostingClassifier()
+
+	gb.fit(X_train, y_train)
+
+	valid_pred = gb.predict(X_valid)
+
+	print "Gradient Boost Evaluation score:", eval(valid_pred, y_valid)
+
+	rfr = RandomForestClassifier()
+
+	rfr.fit(X_train, y_train)
+
+	valid_pred = rfr.predict(X_valid)
+
+	print "Random Forest Evaluation score:", eval(valid_pred, y_valid)
+
+	gb_test_pred = gb.predict(tmp_test)
+	rfr_test_pred = rfr.predict(tmp_test)
+
+	write_to_file("gb_preds.csv", gb_test_pred)
+	write_to_file("rfr_preds.csv", rfr_test_pred)
 
 	# tmp_test = np.zeros((N_test,FEATS,FRAMES))
 	# for i in range(N_test):
