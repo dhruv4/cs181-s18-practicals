cf49eb94417f4b45c7af9af42d69232498fd636b
Dhruv Gupta <dhruv4@gmail.com>
2018-04-05 10:27:14 -0400

    update generate features flag

--- P3/dhruv.py
+++ P3/dhruv.py
@@ -10,6 +10,7 @@ import torch
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.model_selection import GridSearchCV
 
 SAMPLE_RATE = 22050 # number of samples of the audio per second
 
@@ -29,8 +30,7 @@ def extract_feature(X, sample_rate, BINS_OCTAVE, NUM_BINS):
     chroma = np.mean(librosa.feature.chroma_stft(S=stft, sr=sample_rate).T,axis=0)
     mel = np.mean(librosa.feature.melspectrogram(X, sr=sample_rate).T,axis=0)
     contrast = np.mean(librosa.feature.spectral_contrast(S=stft, sr=sample_rate).T,axis=0)
-    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X),
-    sr=sample_rate).T,axis=0)
+    tonnetz = np.mean(librosa.feature.tonnetz(y=librosa.effects.harmonic(X), sr=sample_rate).T,axis=0)
 
     ext_features = np.hstack([mfccs,chroma,mel,contrast,tonnetz])
 
@@ -57,6 +57,7 @@ def tempogram(y, SAMPLE_RATE):
 
 def main():
 
+	GENERATE_FEATURES = True
 	N = None
 
 	# Just some re-shaping and dimension finding
@@ -85,39 +86,42 @@ def main():
 
 	# This means that the spectrograms are 168 rows (frequencies)
 	# By 173 columns (time frames)
-	# song = X_train[0]
-	# test_spec = mel_spec(song, SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-	# print test_spec.shape
-	# FEATS = test_spec.shape[0]
-	# FRAMES = test_spec.shape[1]
+	if GENERATE_FEATURES:
+		song = X_train[0]
+		test_spec = mel_spec(song, SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+		print test_spec.shape
+		FEATS = test_spec.shape[0]
+		FRAMES = test_spec.shape[1]
 
-	# print "features"
+		# print "features"
 
-	# tmp_train = np.zeros((N_train,FEATS,FRAMES))
-	# for i in range(N_train):
-	#     tmp_train[i,:,:] = mel_spec(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+		# tmp_train = np.zeros((N_train,FEATS,FRAMES))
+		# for i in range(N_train):
+		#     tmp_train[i,:,:] = mel_spec(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
 
-	# tmp_train = np.zeros((N_train,193))
+		tmp_train = np.zeros((N_train,193))
 
-	# for i in range(N_train):
-	# 	f = extract_feature(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-	# 	# print f
-	# 	tmp_train[i] = np.array(f)
+		for i in range(N_train):
+			f = extract_feature(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+			# print f
+			tmp_train[i] = np.array(f)
 
-	tmp_test = np.zeros((N_test,193))
+		tmp_test = np.zeros((N_test,193))
 
-	for i in range(N_test):
-		f = extract_feature(test[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-		# print f
-		tmp_test[i] = np.array(f)
+		for i in range(N_test):
+			f = extract_feature(test[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
+			# print f
+			tmp_test[i] = np.array(f)
 
-	# np.save("features-train-dhruv.npy", tmp_train)
+		np.save("features-train-dhruv.npy", tmp_train)
 
-	np.save("features-test-dhruv.npy", tmp_test)
+		np.save("features-test-dhruv.npy", tmp_test)
 
-	tmp_train = np.load("features-train-dhruv.npy")
+	else:
 
-	tmp_test = np.load("features-test-dhruv.npy")
+		tmp_train = np.load("features-train-dhruv.npy")
+
+		tmp_test = np.load("features-test-dhruv.npy")
 
 	X_train, X_valid, y_train, y_valid = train_test_split(tmp_train, y_train, test_size=0.2)
 
@@ -129,19 +133,16 @@ def main():
 
 	print X_train.shape
 
-	# nsamples, nx, ny = X_train.shape
-	# d2_train_dataset = X_train.reshape((nsamples,nx*ny))
-
-	# nsamples, nx, ny = X_valid.shape
-	# d2_valid_dataset = X_valid.reshape((nsamples,nx*ny))
-
-	# regr.fit(X_train, y_train)
-
-	# valid_pred = regr.predict(X_valid)
-
-	# print "Lin Reg Evaluation score:", eval(valid_pred, y_valid)
-
 	gb = GradientBoostingClassifier()
+
+    param_grid = {
+        "max_depth": [3, 10, 200, 1000],
+        "learning_rate": [x / 100.0 for x in range(1, 100, 10)],
+        "max_features": [x / 100.0 for x in range(70, 101, 10)]
+    }
+    mymodel = GridSearchCV(gb, param_grid, verbose=10, n_jobs=-1)
+
+    # TODO: Harshita, I have no idea how this works
 
 	gb.fit(X_train, y_train)
 
@@ -162,10 +163,6 @@ def main():
 
 	write_to_file("gb_preds.csv", gb_test_pred)
 	write_to_file("rfr_preds.csv", rfr_test_pred)
-
-	# tmp_test = np.zeros((N_test,FEATS,FRAMES))
-	# for i in range(N_test):
-	#     tmp_test[i,:,:] = mel_spec(test[i])
 
 if __name__ == '__main__':
 	main()
