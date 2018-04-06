c11ed93f5757ac12b3cf0a24d7828a1762d30c52
harshita-gupta <gupta.harshita.001@gmail.com>
2018-04-06 01:08:51 -0400

    code cleaning

--- P3/dhruv.py
+++ P3/dhruv.py
@@ -65,82 +65,66 @@ def tempogram(y, SAMPLE_RATE):
 
 def main():
 
-    GENERATE_FEATURES = True
+    GENERATE_FEATURES = False
     N = None
 
     # Just some re-shaping and dimension finding
-
-    train = np.array(pd.read_csv("train.csv", header=None, nrows=N))
-    test = np.array(pd.read_csv("test.csv", header=None, nrows=N))
-
-    print "N:", N
-    # train = signal[np.newaxis,:]
-    print "Train shape", train.shape
-    N_train = train.shape[0]
-    N_test = test.shape[0]
-    # NUM_SAMPLES = train.shape[1] - 1
-
-    test = test[:, 1:-1]
-
-    y_train = train[:, -1]
-    y_train = y_train.reshape(N_train, 1)
-
-    # remove first and last two elements to drop the mic
-    X_train = train[:, 1:-2]
-
-    # JUST SOME FOURIER TRANSFORM PARAMETERS
-    BINS_OCTAVE = 12 * 2
-    N_OCTAVES = 7
-    NUM_BINS = BINS_OCTAVE * N_OCTAVES
-
     # This means that the spectrograms are 168 rows (frequencies)
     # By 173 columns (time frames)
     if GENERATE_FEATURES:
+        train = np.array(pd.read_csv("train.csv", header=None, nrows=1))
+        test = np.array(pd.read_csv("test.csv", header=None, nrows=1))
+
+        print "N:", N
+        # train = signal[np.newaxis,:]
+        print "Train shape", train.shape
+        N_train = train.shape[0]
+        N_test = test.shape[0]
+        # NUM_SAMPLES = train.shape[1] - 1
+
+        test = test[:, 1:-1]
+
+        y_train = train[:, -1]
+        y_train = y_train.reshape(N_train, 1)
+
+        # remove first and last two elements to drop the mic
+        X_train = train[:, 1:-2]
+
+        # JUST SOME FOURIER TRANSFORM PARAMETERS
+        BINS_OCTAVE = 12 * 2
+        N_OCTAVES = 7
+        NUM_BINS = BINS_OCTAVE * N_OCTAVES
+
         song = X_train[0]
         test_spec = mel_spec(song, SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
         print test_spec.shape
-        # FEATS = test_spec.shape[0]
-        # FRAMES = test_spec.shape[1]
-
-        # print "features"
-
-        # tmp_train = np.zeros((N_train,FEATS,FRAMES))
-        # for i in range(N_train):
-        #     tmp_train[i,:,:] = mel_spec(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
 
         tmp_train = np.zeros((N_train, 193))
 
         for i in range(N_train):
             f = extract_feature(X_train[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-            # print f
             tmp_train[i] = np.array(f)
 
         tmp_test = np.zeros((N_test, 193))
 
         for i in range(N_test):
             f = extract_feature(test[i], SAMPLE_RATE, BINS_OCTAVE, NUM_BINS)
-            # print f
             tmp_test[i] = np.array(f)
 
         np.save("features-train-dhruv.npy", tmp_train)
-
+        np.save("y-train-features-dhruv.npy", y_train)
         np.save("features-test-dhruv.npy", tmp_test)
 
     else:
-
         tmp_train = np.load("features-train-dhruv.npy")
-
+        y_train = np.load("y-train-features-dhruv.npy")
         tmp_test = np.load("features-test-dhruv.npy")
+
 
     X_train, X_valid, y_train, y_valid = train_test_split(
         tmp_train, y_train, test_size=0.01)
 
-    # regr = LinearRegression()
-
     print "fitting"
-
-    # print X_train[0], y_train[0]
-
     print X_train.shape
 
     param_grid = {
