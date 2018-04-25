e5e809e55a37ee7c091dcd19210e7616f992806d
Michael Wornow <mwornow98@gmail.com>
2018-04-25 16:03:31 -0400

    More Qlearning stuff

--- P4/stub.py
+++ P4/stub.py
@@ -46,6 +46,7 @@ class QLearner(object):
     def __init__(self):
         self.LEARNING_RATE = 0.9
         self.DISCOUNT = 0.9
+        self.A = [0, 1]
         self.last_state  = None
         self.last_action = None
         self.last_reward = None
@@ -57,6 +58,15 @@ class QLearner(object):
         self.last_reward = None
         self.q = {}
 
+    def discrete_state(self, state):
+        tree_dist = state['tree']['dist']
+        tree_top = state['tree']['top']
+        tree_bot = state['tree']['bot']
+        monkey_vel = state['monkey']['vel']
+        monkey_top = state['monkey']['top']
+        monkey_bot = state['monkey']['bot']
+        return s
+
     def action_callback(self, state):
         '''
         Implement this function to learn things and take actions.
@@ -65,14 +75,23 @@ class QLearner(object):
         # You might do some learning here based on the current state and the last state.
         # You'll need to select an action and return it.
         # Return 0 to swing and 1 to jump.
+
+        # At state s, take action a, end up in state s'
+        s = self.discrete_state(state)
+        a = self.last_action
+        s_prime = self.discrete_state(self.last_state)
+
         if s,a not in self.q:
             self.q[s,a] = 0
-        self.q[s,a] = (1 - self.LEARNING_RATE) * self.q[s,a] + self.LEARNING_RATE( r(s,a,s'') + self.DISCOUNT max_a''Q(s'', a''))
-        new_action = npr.rand() < 0.1
+        max_q = np.max(self.q[s_prime, 0], self.q[s_prime, 1])
+        self.q[s,a] = (1 - self.LEARNING_RATE) * self.q[s,a] + self.LEARNING_RATE( self.last_reward + self.DISCOUNT * max_q)
+        
+        new_action = np.argmax(self.q[s_prime,0], self.q[s_prime,1])
         new_state  = state
+
         self.last_action = new_action
         self.last_state  = new_state
-        return self.last_action
+        return new_action
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
