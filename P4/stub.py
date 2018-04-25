8191ebb8bf808b3ae5acf027f77d5d3e3c912c49
Michael Wornow <mwornow98@gmail.com>
2018-04-25 12:37:52 -0400

    Initial QLearner

--- P4/stub.py
+++ P4/stub.py
@@ -5,7 +5,7 @@ import numpy.random as npr
 from SwingyMonkey import SwingyMonkey
 
 
-class Learner(object):
+class Random(object):
     '''
     This agent jumps randomly.
     '''
@@ -25,23 +25,57 @@ class Learner(object):
         Implement this function to learn things and take actions.
         Return 0 if you don't want to jump and 1 if you do.
         '''
-
         # You might do some learning here based on the current state and the last state.
-
         # You'll need to select and action and return it.
         # Return 0 to swing and 1 to jump.
-
         new_action = npr.rand() < 0.1
         new_state  = state
-
         self.last_action = new_action
         self.last_state  = new_state
-
         return self.last_action
 
     def reward_callback(self, reward):
         '''This gets called so you can see what reward you get.'''
+        self.last_reward = reward
 
+class QLearner(object):
+    '''
+    This agent jumps randomly.
+    '''
+
+    def __init__(self):
+        self.LEARNING_RATE = 0.9
+        self.DISCOUNT = 0.9
+        self.last_state  = None
+        self.last_action = None
+        self.last_reward = None
+        self.q = {}
+
+    def reset(self):
+        self.last_state  = None
+        self.last_action = None
+        self.last_reward = None
+        self.q = {}
+
+    def action_callback(self, state):
+        '''
+        Implement this function to learn things and take actions.
+        Return 0 if you don't want to jump and 1 if you do.
+        '''
+        # You might do some learning here based on the current state and the last state.
+        # You'll need to select an action and return it.
+        # Return 0 to swing and 1 to jump.
+        if s,a not in self.q:
+            self.q[s,a] = 0
+        self.q[s,a] = (1 - self.LEARNING_RATE) * self.q[s,a] + self.LEARNING_RATE( r(s,a,s'') + self.DISCOUNT max_a''Q(s'', a''))
+        new_action = npr.rand() < 0.1
+        new_state  = state
+        self.last_action = new_action
+        self.last_state  = new_state
+        return self.last_action
+
+    def reward_callback(self, reward):
+        '''This gets called so you can see what reward you get.'''
         self.last_reward = reward
 
 
@@ -49,7 +83,6 @@ def run_games(learner, hist, iters = 100, t_len = 100):
     '''
     Driver function to simulate learning by having the agent play a sequence of games.
     '''
-    
     for ii in range(iters):
         # Make a new monkey object.
         swing = SwingyMonkey(sound=False,                  # Don't play sounds.
@@ -57,24 +90,20 @@ def run_games(learner, hist, iters = 100, t_len = 100):
                              tick_length = t_len,          # Make game ticks super fast.
                              action_callback=learner.action_callback,
                              reward_callback=learner.reward_callback)
-
         # Loop until you hit something.
         while swing.game_loop():
             pass
-        
         # Save score history.
         hist.append(swing.score)
-
         # Reset the state of the learner.
         learner.reset()
-        
     return
 
 
 if __name__ == '__main__':
 
 	# Select agent.
-	agent = Learner()
+	agent = Random()
 
 	# Empty list to save history.
 	hist = []
